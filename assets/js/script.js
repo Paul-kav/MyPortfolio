db3823636d0c9e34a68be7faf5dfafeb0806f8b4
Paul-kav <paulkavuma82@gmail.com>
2023-07-29 05:18:47 -0400

    Added javascript file and js for the sidebar

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+let navLinks = document.querySelectorAll("nav ul li a");
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", function() {
+      // 1. Remove "active" class from the currently active link in navigation
+      document.querySelector("nav ul li a.active").classList.remove("active");
+  
+      // 2. Add "active" class to the clicked link
+      link.classList.add("active");
+  
+      // 3. Remove "active" class from the currently active section in main
+      document.querySelector("main > section.active").classList.remove("active");
+  
+      // 4. Add "active" class to the section associated with the clicked link
+      document.querySelector(`main > section${link.getAttribute('href')}`).classList.add("active");
+    });
+  });
+  
